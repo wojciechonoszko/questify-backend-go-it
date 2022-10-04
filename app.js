4233d9fec1301d2e3792cf9b45dd1c41b46faab7
Wojciech <wojciechonoszko@gmail.com>
2022-10-04 22:14:54 +0200

    add files

--- app.js
+++ app.js
@@ -1,25 +1,76 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const contactsRouter = require('./routes/api/contacts')
+const rateLimit = require("express-rate-limit");
+const boolParser = require("express-query-boolean");
+const helmet = require("helmet");
 
-const app = express()
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger/swagger.json");
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const { httpStatusCodes } = require("./helpers/httpstatuscodes.js");
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const usersRouter = require("./routes/usersRoutes");
+const cardsRouter = require("./routes/cardsRoutes");
 
-app.use('/api/contacts', contactsRouter)
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(helmet());
+
+app.get("env") !== "test" && app.use(logger(formatsLogger));
+
+app.use(express.static("public"));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  handler: (req, res, next) => {
+    return res.status(httpStatusCodes.TOO_MANY_REQUESTS).json({
+      status: "error",
+      code: httpStatusCodes.TOO_MANY_REQUESTS,
+      message: "Too Many Requests",
+      result: "HTTP_TOO_MANY_REQUESTS",
+    });
+  },
+});
+app.use(limiter);
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS",
+    preflightContinue: false,
+    optionsSuccessStatus: httpStatusCodes.NO_CONTENT,
+  })
+);
+app.use(express.json({ limit: 100000 }));
+app.use(boolParser());
+
+app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use("/users", usersRouter);
+app.use("/cards", cardsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(httpStatusCodes.NOT_FOUND).json({
+    status: "error",
+    code: httpStatusCodes.NOT_FOUND,
+    message: `Use api on routes ${req.baseUrl}/cards or ${req.baseUrl}/users.`,
+    result: "Not Found",
+  });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  err.code = err.code ? err.code : httpStatusCodes.INTERNAL_SERVER_ERROR;
+  res.status(err.code).json({
+    status: err.code === 500 ? "fail" : "error",
+    code: err.code,
+    message: err.message,
+    result: err.code === 500 ? "Internal Server Error" : err.result,
+  });
+});
 
-module.exports = app
+module.exports = app;
